8512a2e06ff8c2cd09dae0955861ca0f5cb5be3f
LagSeeN <19259262+LagSeeN@users.noreply.github.com>
2021-03-13 15:23:57 +0700

    :truck: change app name android

--- android/app/src/main/java/com/lagseen/tnicoop/MainApplication.java
+++ android/app/src/main/java/com/lagseen/tnicoop/MainApplication.java
@@ -1,4 +1,4 @@
-package com.tnicoop_mobile;
+package com.lagseen.tnicoop;
 
 import android.app.Application;
 import android.content.Context;
@@ -65,7 +65,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.tnicoop_mobile.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.lagseen.tnicoop.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
